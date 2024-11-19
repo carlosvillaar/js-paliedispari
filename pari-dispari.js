565b40e29a9410cb696f53d5c801c7d1f1995abe
carlosvillaar <carlosvillar004@gmail.com>
2024-11-19 19:07:11 +0100

    fix: correct syntax errors

--- pari-dispari.js
+++ pari-dispari.js
@@ -2,7 +2,6 @@
 L’utente sceglie pari o dispari e inserisce un numero da 1 a 5. Generiamo un numero random (sempre da 1 a 5) per il computer (usando una funzione). Sommiamo i due numeri Stabiliamo se la somma dei due numeri è pari o dispari (usando una funzione) Dichiariamo chi ha vinto.
 */
 
-// 1.
 const userNumber = prompt(`Inserisci un numero da 1 a 5`);
 console.log(`Tu hai scelto il numero ` + parseInt(userNumber));
 
